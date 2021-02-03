cdbee38ace2698a9d763e43dfabf78b0d50bb4f4
Wojtek Wydmuch <wojciechwydmuch73@gmail.com>
2021-02-03 22:20:33 +0100

    Added home.html, bugs fixed with HTTP requests

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ import bcrypt
 
 app = Flask(__name__)
 
-db_connection = mysql.connector.connect(
+connection = mysql.connector.connect(
 host="localhost",
 user="root",
 passwd="nowe_haslo",
 database="event_app_database"
 )
 
+cur = connection.cursor()
 class User:
     def __init__(self, name, email, hash_password):
         self.name = name
@@ -19,38 +20,55 @@ class User:
 
 app = Flask(__name__)
 
-@app.route('/login')
-@app.route('/', methods=['POST','GET'])
+@app.route('/')
+def home():
+    return render_template("login.html")
+
+@app.route('/login', methods=['POST','GET'])
 def login():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['pass']
-        db_connection.execute('SELECT email FROM users WHERE email=?', (email))
-        db_connection.execute('SELECT password FROM users WHERE password=?', (password))
-        check_email = db_connection.fetchone()
-        check_password = db_connection.fetchone()
-        if check_email !=0 and check_password !=0:
+        cur.execute("SELECT email FROM users WHERE email=%s", (email,))
+        data = cur.fetchone()
+        
+       
+        
+        # cursor_email = connection.cursor()
+        # cursor_password = connection.cursor()
+
+        # cursor_email.execute(sql_query_email)
+        # cursor_password.execute(sql_query_password)
+
+        # records_email = cursor_email.fetchone()
+        # records_password = cursor_password.fetchone()
+
+        if data != None:
+            return render_template("home.html")
+        if data == None:
             return render_template("register.html")
     else:
         return render_template("login.html")
+    
 
-@app.route('/register', methods=["GET", "POST"])
+@app.route('/register', methods=["GET","POST"])
 def register():
     if request.method == 'GET':
         return render_template("register.html")
-    else:
-        name = request.form['name']
-        email = request.form['email']
-        password = request.form['password'].encode('utf-8')
-        hash_password = bcrypt.hashpw(password, bcrypt.gensalt())
-        User(name, email, hash_password)
+    # else:
+    #     name = request.form['name']
+    #     email = request.form['email']
+    #     password = request.form['password'].encode('utf-8')
+    #     hash_password = bcrypt.hashpw(password, bcrypt.gensalt())
+    #     User(name, email, hash_password)
 
         
-        db_connection.execute("INSERT INTO Users (name, email, password) VALUES (%s,%s,%s)",(User.name,User.email,User.hash_password))
-        mysql.connection.commit()
-        session['name'] = request.form['name']
-        session['email'] = request.form['email']
-        return redirect(url_for('home'))
+    #     # db_connection.execute("INSERT INTO Users (name, email, password) VALUES (%s,%s,%s)",(User.name,User.email,User.hash_password))
+    #     # mysql.connection.commit()
+    #     session['name'] = request.form['name']
+    #     session['email'] = request.form['email']
+    else:
+        return redirect("login.html")
 
 if __name__ == '__main__':
     app.secret_key = "^A%DEventApp"
